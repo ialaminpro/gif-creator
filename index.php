420c19e5f07410c58d7120fe55dbfcbe3f62c27d
Al Amin <ialamin.pro@gmail.com>
2022-01-04 20:13:51 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'src/GifCreator/GifCreator.php';
+// Create an array containing file paths, resource var (initialized with imagecreatefromXXX), 
+// image URLs or even binary code from image files.
+// All sorted in order to appear.
+$frames = array(
+    imagecreatefrompng("images/qudb.png"), // Resource var
+    "images/pic1.jpg", // Image file path
+    //file_get_contents("./images/pic3.jpg"), // Binary source code
+    //'http://thisisafakedomain.com/images/pic4.jpg', // URL,
+);
+
+// Create an array containing the duration (in millisecond) of each frames (in order too)
+$durations = array(40, 80, 40, 20);
+
+// Initialize and create the GIF !
+$gc = new GifCreator();
+$gc->create($frames, $durations, 5);
+
+$gifBinary = $gc->getGif();
+// header('Content-type: image/gif');
+// header('Content-Disposition: filename="butterfly.gif"');
+// echo $gifBinary;
+// exit;
+
+file_put_contents('animated_picture.gif', $gifBinary);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'sybio/gif-creator',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sybio/gif-creator' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
